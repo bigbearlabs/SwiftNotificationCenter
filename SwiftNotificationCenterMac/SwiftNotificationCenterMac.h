dc3774eaaa991dcce6e76087d43706c07829973c
Andy Park <andy@bigbearlabs.com>
2017-03-01 12:01:11 +0100

    add a scheme for mac.

---
+++ SwiftNotificationCenterMac/SwiftNotificationCenterMac.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNotificationCenterMac.h
+//  SwiftNotificationCenterMac
+//
+//  Created by ilo on 01/03/2017.
+//  Copyright © 2017 Mango. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftNotificationCenterMac.
+FOUNDATION_EXPORT double SwiftNotificationCenterMacVersionNumber;
+
+//! Project version string for SwiftNotificationCenterMac.
+FOUNDATION_EXPORT const unsigned char SwiftNotificationCenterMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNotificationCenterMac/PublicHeader.h>
+
+
